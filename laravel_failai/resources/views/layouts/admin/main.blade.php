805f184b768601a6fcc5429af456b8df716befd9
JustasMe97 <meskauskasjustas@gmail.com>
2023-02-19 22:33:52 +0200

    Moving some blades to Boostrap framework

--- laravel_failai/resources/views/layouts/admin/main.blade.php
+++ laravel_failai/resources/views/layouts/admin/main.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -12,19 +11,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="icon" type="image/svg+xml" href="{{asset('favicon.svg')}}">
     <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    {{--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">--}}
+    {{--    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">--}}
     <link href="{{asset('/css/style.css')}}" rel="stylesheet">
     <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/themes/light.css"/>
-    <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/shoelace.js"></script>
-    <link rel="stylesheet" href="{{asset('/css/app.css')}}" />
+    {{--    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>--}}
+    {{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/themes/light.css"/>--}}
+    {{--    <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.0.0/dist/shoelace.js"></script>--}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-<img src="{{asset('/img/background.png')}}" class="full_fit" alt="">
+{{--<img src="{{asset('/img/kalnas.jpg')}}" class="full_fit" alt="">--}}
 <div class="main_grid">
-    @include('layouts.admin.header')
+    @include('layouts.admin.headerNew')
     <div class="container">
         @if ($errors->any())
             @foreach ($errors->all() as $error)
@@ -34,8 +33,9 @@
         @yield('content', 'Default content')
     </div>
     <br>
-    @include('layouts.admin.footer')
+    @include('layouts.admin.footerNew')
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
 <script src="{{asset('/js/mano.js')}}"></script>
 </body>
 </html>
--- laravel_failai/resources/views/products/show.blade.php
+++ laravel_failai/resources/views/products/show.blade.php
@@ -4,12 +4,10 @@
 
 @section('content')
     <div class="row">
-        <div class="col s12 m3">
-            <div class="card">
-                <div class="card-image">
-                    <img src="{{$product->image}}" alt="produktas">
-                    <span class="card-title">{{ $product->name }}</span>
-                </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="{{$product->image}}" alt="produktas">
+            <div class="card-body">
+                <h5 class="card-title">{{ $product->name }}</h5>
                 <div class="card-content">
                     <div>ID: {{$product->id}}</div>
                     <p>Price: {{ $product->price }}</p>
@@ -19,19 +17,39 @@
                     <p>Last updated: {{ $product->updated_at }}</p>
                 </div>
                 <div class="card-action">
-                    <a href="{{ route('products.edit', $product->id) }}"
-                       data-tooltip="Redaguoti"
-                       class="tooltipped waves-effect waves-light btn-small blue darken-4">
-                        <i class="tiny material-icons">edit</i>
-                    </a>
-                    <form action="{{ route('products.destroy', $product->id) }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" data-tooltip="Delete"
-                                class="tooltipped waves-effect waves-light btn-small blue darken-4">
-                            <i class="tiny material-icons">delete</i>
-                        </button>
-                    </form>
+                    <div class="d-flex flex-row">
+                        <a role="button" class="btn btn-dark mr-1 ml-1" href="{{route('products.edit', $product->id)}}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 class="bi bi-pen" viewBox="0 0 16 16">
+                                <path
+                                    d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"></path>
+                            </svg>
+                            Edit
+                        </a>
+                        {{--                            <a href="{{route('orders.edit', $order->id)}}" class="btn blue darken-4 me-5">Edit</a>--}}
+                        <form action="{{route('products.destroy', $product->id)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            {{--                                <button type="submit" class="btn blue darken-4 mr-1">Delete</button>--}}
+                            <button type="submit" class="btn btn-danger mr-1 ml-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                     fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path
+                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                    <path fill-rule="evenodd"
+                                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                                </svg>
+                                Delete
+                            </button>
+                        </form>
+                        <a role="button" class="btn btn-secondary mr-1 ml-1" href="{{route('products.index')}}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+                                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"></path>
+                                <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"></path>
+                            </svg>
+                            Back
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
